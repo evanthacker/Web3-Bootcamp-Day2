ee18cfa97873a2c18ad8bece4ce2a93dc6dd0e2f
Evan Thacker <25277597+evanthacker@users.noreply.github.com>
2022-12-24 12:04:42 -0500

    Create day2.sol

---
+++ day2.sol
@@ -0,0 +1,6 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+//pragma solidity 0.8.10; exact
+// pragma solidity ^0.8.10; up until the next minor version
+// pragma solidity >= 0.8.0 < 0.9.5; in between two versions
